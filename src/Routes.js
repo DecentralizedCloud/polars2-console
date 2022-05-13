2b23246ac6c839780317bffa05d2ff8ecec09739
siddheshkotkar <kotkar.sidh@gmail.com>
2022-05-14 00:24:21 +0530

    final deployment

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Login from "./Login";
 
